481d9ab61a687baf43cdd431a8a68a83fbc198cb
Timothy Williams <Timothy.Williams@stu.glasgow.kyschools.us>
2024-09-19 09:17:32 -0500

    Initial Commit

---
+++ Unit conversions/Unit_conversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_conversionsApp.swift
+//  Unit conversions
+//
+//  Created by Student on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Unit_conversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
