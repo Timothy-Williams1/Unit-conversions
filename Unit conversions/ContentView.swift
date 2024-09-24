ed820458fadd39fc1de0d8b6f8e1b02ee66f295c
Timothy Williams <Timothy.Williams@stu.glasgow.kyschools.us>
2024-09-24 09:13:55 -0500

    Cpmpleted project

--- Unit conversions/ContentView.swift
+++ Unit conversions/ContentView.swift
@@ -8,14 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State private var startingTemp = 0.0
+    @State private var endingTemp = 0.0
+    @State private var startingMeasurment = "Celsius"
+    @State private var endingMeasurment = "Celsius"
+    @FocusState private var amountIsFocused: Bool
+    
+    let measurments = ["Celsius","Fahrenheit","Kelven"]
+    
+    var convertingTemp: Double {
+        var newTemp = startingTemp
+    
+  
+    if startingMeasurment == "Celsius" &&
+        endingMeasurment == "Celsius" {
+            return newTemp
         }
-        .padding()
+    
+    if startingMeasurment == "Fahrenheit" &&
+        endingMeasurment == "Fahrenheit" {
+            return newTemp
+        }
+        if startingMeasurment == "kelven" &&
+            endingMeasurment == "Kelven" {
+                return newTemp
+            }
+    // Skipping Celsius (conv others to Cels)
+        if startingMeasurment == "Fahrenheit" {
+            newTemp = (newTemp - 32) / 1.8
+        } else if startingMeasurment == "Kelven" {
+            newTemp = newTemp - 273.15
+        }
+        
+        // conv from Cels to Fahr or kel
+        if endingMeasurment == "Fahrenheit" {
+            newTemp = newTemp * 1.8 + 32
+        } else if endingMeasurment == "Kelven" {
+            newTemp = newTemp + 273.15
+        }
+      // returnes coverted value
+    return newTemp
+}
+
+var body: some View {
+    NavigationStack {
+        Form{
+            Section("Select a starting temperature"){
+                Picker("Temperature Measurement",selection: $startingMeasurment){
+                    ForEach(measurments, id: \.self){
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                TextField("Temperature", value: $startingTemp, format: .number)
+                    .keyboardType(.numberPad)
+                    .focused($amountIsFocused)
+            }
+            
+            Section("Select a measuerment to convert to"){
+                Picker("Temperature Measurement",selection: $endingMeasurment){
+                    ForEach(measurments, id: \.self){
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                Text(convertingTemp, format: .number)
+            }
+            .navigationTitle("Temperature Converter")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done"){
+                        amountIsFocused = false
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
